41ecb352ddca57015292e44e17a002a4490cc52b
fbetteo <francobetteo@gmail.com>
2020-07-07 01:48:57 -0300

    test set

--- model.py
+++ model.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import numpy as np
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score
 
-laptimes = pd.read_csv("data/lap_times.csv")
-races = pd.read_csv("data/races.csv")
+# data
 results = pd.read_csv("data/results.csv")
 
-df.head(20)
-df.describe()
+# cleaning
 df = results[['grid', 'position']]
 df = df[(df.position != "\\N") & (df.grid != 0)]
 df['position'] = df['position'].astype('int32')
@@ -19,6 +18,13 @@ df['position'] = np.where(df['position'] > 1, 2,1)
 X = df.drop(columns = "position")
 y = df['position']
 
+# isolating test
+test_index = X.shape[0]-1000 # ultimos 1000
+X_test = X.iloc[test_index:]
+y_test = y.iloc[test_index:]
+X = X[:test_index]
+y = y[:test_index]
+
 
 clf = DecisionTreeClassifier()
 clf = clf.fit(X,y)
@@ -26,8 +32,7 @@ clf = clf.fit(X,y)
 y_predict = clf.predict(X )
 
 accuracy_score(y, y_predict)
-
-from sklearn.metrics import confusion_matrix
+roc_auc_score(y, y_predict)
 
 pd.DataFrame(
     confusion_matrix(y, y_predict), columns = ["Pred First", "Pred Not First"],
