17620f83c879743701ef61bfe998882cfdf2e8c1
Franco <francobetteo@gmail.com>
2020-07-08 01:07:36 -0300

    test

--- model.py
+++ model.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics.classification import fbeta_score
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score, precision_recall_fscore_support
+
+val_size = 0.3
+seed = 8
 
 # data
 results = pd.read_csv("data/results.csv")
@@ -13,34 +17,47 @@ df = df[(df.position != "\\N") & (df.grid != 0)]
 df['position'] = df['position'].astype('int32')
 
 # classify first place
-df['position'] = np.where(df['position'] > 1, 2,1)
+df['position'] = np.where(df['position'] > 1, 2, 1)
 
-X = df.drop(columns = "position")
+X = df.drop(columns="position")
 y = df['position']
 
 # isolating test
-test_index = X.shape[0]-1000 # ultimos 1000
+test_index = X.shape[0]-1000  # ultimos 1000
 X_test = X.iloc[test_index:]
 y_test = y.iloc[test_index:]
 X = X[:test_index]
 y = y[:test_index]
 
+# train validation
+X_train, X_val, y_train,  y_val = train_test_split(X, y, test_size=val_size,
+                                                   random_state=seed)
 
+# model
 clf = DecisionTreeClassifier()
-clf = clf.fit(X,y)
+clf = clf.fit(X_train, y_train)
 
-y_predict = clf.predict(X )
+# validation
+y_predict = clf.predict(X_val)
 
-accuracy_score(y, y_predict)
-roc_auc_score(y, y_predict)
+precision_val, recall_val, fbeta_val, support_val = precision_recall_fscore_support(y_val, y_predict)
+
+precision_val
+
+accuracy_score(y_val, y_predict)
+roc_auc_score(y_val, y_predict)
 
 pd.DataFrame(
-    confusion_matrix(y, y_predict), columns = ["Pred First", "Pred Not First"],
-    index = ["True First", "True Not First"]
+    confusion_matrix(y_val, y_predict), columns=["Pred First", "Pred Not First"],
+    index=["True First", "True Not First"]
 )
 
-df.describe()
-df.shape
+# test 
+y_predict_test = clf.predict(X_test)
+precision_test, recall_test, fbeta_test, support_test = precision_recall_fscore_support(y_test, y_predict_test)
+precision_test
 
-y.shape
-
+pd.DataFrame(
+    confusion_matrix(y_test, y_predict_test), columns=["Pred First", "Pred Not First"],
+    index=["True First", "True Not First"]
+)
